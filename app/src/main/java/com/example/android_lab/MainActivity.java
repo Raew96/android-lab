5d170387f3492d50da0a94fb7f2e4fa1561cc6ed
Rafal <rafal.biarda1996@gmail.com>
2020-04-16 21:00:57 +0200

    Create ListView and ArrayAdapter

--- app/src/main/java/com/example/android_lab/MainActivity.java
+++ app/src/main/java/com/example/android_lab/MainActivity.java
@@ -3,13 +3,33 @@ package com.example.android_lab;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
 
-    //TODO Cos trzeba zrobic :/
+    private ArrayList<String> target;
+    private ArrayAdapter adapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String [] values = new String[]
+                {
+                        "Kot", "Koń", "Gołab", "Kruk", "Dzik", "Karp", "Osioł", "Chomik", "Mysz", "Jez", "Karaluch"
+                };
+
+        this.target = new ArrayList<>();
+        this.target.addAll(Arrays.asList(values));
+
+        this.adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, this.target );
+
+        ListView listView = (ListView)findViewById(R.id.list_view);
+        listView.setAdapter(this.adapter);
     }
 }
